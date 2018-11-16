4a1fbb4ba70ca318b42049fccccdd54a36caf6e6
linkin123 <32915971+linkin123@users.noreply.github.com>
2018-11-16 12:46:27 -0300

    texto1 rama master

--- app/src/main/java/com/example/administrador/drawer/MainActivity.java
+++ app/src/main/java/com/example/administrador/drawer/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.administrador.drawer;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        TextView text1 = findViewById(R.id.text_uno);
+        TextView text2 = findViewById(R.id.text_dos);
+        text1.setText("texto en rama master");
+        
     }
 }
