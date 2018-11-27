b3c5dff170388ce135eb2b40db648cf0f71792ea
linkin123 <32915971+linkin123@users.noreply.github.com>
2018-11-27 11:42:54 -0300

    pruebas 27112018

--- app/src/main/java/com/example/administrador/drawer/MainActivity.java
+++ app/src/main/java/com/example/administrador/drawer/MainActivity.java
@@ -2,27 +2,75 @@ package com.example.administrador.drawer;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationSet;
+import android.view.animation.LayoutAnimationController;
+import android.view.animation.TranslateAnimation;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private boolean bandera = true;
+    private LinearLayout layoutAnimado;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        layoutAnimado = (LinearLayout) findViewById(R.id.animado);
 
-        TextView text1 = findViewById(R.id.text_uno);
-        TextView text2 = findViewById(R.id.text_dos);
-        text1.setText("texto en rama modificaciones");
-        text2.setText("texto 2 en rama modificaciones");
+    }
+
+    public void mostrar(View button)
+    {
+        if (layoutAnimado.getVisibility() == View.GONE)
+        {
+            bandera = false;
+            animar(true);
+            layoutAnimado.setVisibility(View.VISIBLE);
+        }
+    }
+
+    public void ocultar(View button)
+    {
+        if (layoutAnimado.getVisibility() == View.VISIBLE)
+        {
+            bandera = true;
+            animar(false);
+            layoutAnimado.setVisibility(View.GONE);
+        }
+
+    }
+
+    private void animar(boolean mostrar)
+    {
+        AnimationSet set = new AnimationSet(true);
+        Animation animation = null;
+        if (mostrar)
+        {
+            //desde la esquina inferior derecha a la superior izquierda
+            animation = new TranslateAnimation(Animation.RELATIVE_TO_SELF, 1.0f, Animation.RELATIVE_TO_SELF, 0.0f, Animation.RELATIVE_TO_SELF, 1.0f, Animation.RELATIVE_TO_SELF, 0.0f);
+        }
+        else
+        {    //desde la esquina superior izquierda a la esquina inferior derecha
+            animation = new TranslateAnimation(Animation.RELATIVE_TO_SELF, 0.0f, Animation.RELATIVE_TO_SELF, 1.0f, Animation.RELATIVE_TO_SELF, 0.0f, Animation.RELATIVE_TO_SELF, 1.0f);
+        }
+        //duración en milisegundos
+        animation.setDuration(800);
+        set.addAnimation(animation);
+        LayoutAnimationController controller = new LayoutAnimationController(set, 0.25f);
+
+        layoutAnimado.setLayoutAnimation(controller);
+        layoutAnimado.startAnimation(animation);
+
+        layoutAnimado.setLayoutAnimation(controller);
+        layoutAnimado.startAnimation(animation);
 
 
-        text1.setText("texto en rama modificaciones");
-        text2.setText("texto 2 en rama modificaciones");
-
-
-        text1.setText("texto en rama modificaciones con merge ?????");
-        text2.setText("texto 2 en rama modificaciones");
+        layoutAnimado.setLayoutAnimation(controller);
+        layoutAnimado.startAnimation(animation);
 
     }
 }
